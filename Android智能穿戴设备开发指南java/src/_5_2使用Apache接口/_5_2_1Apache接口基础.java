b7807fb0097adeac36677687a69b38771ba3ba38
1040003585 <1040003585@qq.com>
2016-06-08 21:11:50 +0800

    ...

---
+++ Android智能穿戴设备开发指南java/src/_5_2使用Apache接口/_5_2_1Apache接口基础.java
@@ -0,0 +1,14 @@
+package _5_2使用Apache接口;
+
+import sun.rmi.runtime.Log;
+
+import com.sun.security.ntlm.Client;
+
+public class _5_2_1Apache接口基础 {
+
+	public static void main(String[] args) {
+		//HttpGet request = new HttpGet("https://www.baidu.com");
+		//HttpResponse reponse=client.execute(request);
+		
+	}
+}
